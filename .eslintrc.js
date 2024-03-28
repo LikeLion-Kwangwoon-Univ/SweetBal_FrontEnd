f0e130dc77673fe3a6404c8471c8a254befca312
DevTeddy123 <ab315601@naver.com>
2024-03-28 16:41:59 +0900

    Docs : eslint, prettier 주석 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,30 @@
+// 코드 컨벤션에 위배되는 코드 혹은 문법적인 오류나 안티 패턴을 찾아줌
+// 즉, 코드의 퀄리티를 보장한다
+
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: "script",
+        sourceType: 'script',
       },
     },
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser', //eslint가 TS문법을 이해할 수 있게 해준다
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint", "react"],
+  plugins: ['@typescript-eslint', 'react'],
   rules: {
-    "react/react-in-jsx-scope": "off",
+    'react/react-in-jsx-scope': 'off',
   },
-};
+}
