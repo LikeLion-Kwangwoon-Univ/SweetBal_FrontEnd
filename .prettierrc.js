c487c492a0caafd78496451d7a083a46cfe5280f
DevTeddy123 <ab315601@naver.com>
2024-03-28 12:27:00 +0900

    Chore : eslint, prettier 설정

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: true,
+  semi: false,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  printWidth: 120,
+  arrowParens: "always",
+  orderedImports: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-	return <>App page</>
+  return <>App page</>
 }
 
 export default App
