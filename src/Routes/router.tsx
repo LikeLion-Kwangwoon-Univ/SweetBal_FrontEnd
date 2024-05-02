c785aad9de70fdc0bd36f10ed2066b5eda9a9088
DevTeddy123 <ab315601@naver.com>
2024-05-03 02:02:21 +0900

    Feat : react router 설정

--- src/Routes/router.tsx
+++ src/Routes/router.tsx
@@ -1,10 +1,16 @@
 import Header from '../components/layout/header'
+import MainPage from '../pages/main/main'
 
 const router = [
 	{
 		path: '/',
 		element: <Header />,
-		children: [],
+		children: [
+			{
+				path: '/',
+				element: <MainPage />,
+			},
+		],
 	},
 ]
 
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,17 +1,21 @@
 import { styled } from 'styled-components'
 import { FlexCenterCSS } from '../../styles/common'
+import { Outlet } from 'react-router-dom'
 
 function Header() {
-  return (
-    <Container>
-      <h1>로고 자리</h1>
-    </Container>
-  )
+	return (
+		<>
+			<Container>
+				<h1>로고 자리</h1>
+			</Container>
+			<Outlet />
+		</>
+	)
 }
 export default Header
 
 const Container = styled.div`
-  height: 200px;
-  ${FlexCenterCSS}
-  background-color: ${({ theme }) => theme.COLOR.blue4};
+	height: 200px;
+	${FlexCenterCSS}
+	background-color: ${({ theme }) => theme.COLOR.blue4};
 `
