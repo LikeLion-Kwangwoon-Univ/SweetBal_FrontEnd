d4c98664ac9b476f91ce7b5e3a49e5f8159f3865
DevTeddy123 <ab315601@naver.com>
2024-05-09 08:44:10 +0900

    Refactor : MSW, API, Query

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,14 +1,25 @@
 import { styled } from 'styled-components'
 import Group4x4ListBox from './components/group4x4ListBox'
+import useGetMainData from '../../query/get/useGetMain'
+import LoadingPage from '../../components/loading/Loading'
 
 function MainPage() {
+	const { data, isLoading } = useGetMainData()
+
+	if (isLoading)
+		return (
+			<Container>
+				<LoadingPage />
+			</Container>
+		)
+
+	if (data?.length === 0) return <Container>데이터가없네요?</Container>
+
 	return (
 		<Container>
-			{Array(3)
-				.fill(0)
-				.map(() => (
-					<Group4x4ListBox />
-				))}
+			{data?.map(() => (
+				<Group4x4ListBox />
+			))}
 		</Container>
 	)
 }
