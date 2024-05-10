c6258be6d6ea4c3de48ea2a5f8df1fc6ea4c2fae
김동균 <wooheuy5752@nate.com>
2024-05-10 16:27:14 +0900

    Feat : 댓글 페이지에 필요한 api 작성

---
+++ src/apis/commentsApi.ts
@@ -0,0 +1,34 @@
+import axiosInstance from "./@core";
+
+const PATH = "/goldbalance/";
+
+interface commentsType {
+  postId: number;
+  commentId?: number;
+}
+
+interface postLikedType extends commentsType {
+  like: number;
+}
+
+const CommentsApi = {
+  getComments(postId: number) {
+    return axiosInstance.get(`${PATH}/${postId}/comment`);
+  },
+  getRecomments({ postId, commentId }: commentsType) {
+    return axiosInstance.get(`${PATH}/${postId}/recomment/${commentId}`);
+  },
+  postLiked({ postId, commentId, like }: postLikedType) {
+    return axiosInstance.post(
+      `${PATH}/${postId}/comment/${commentId}/liked/${like}`
+    );
+  },
+  postComment({ postId }: commentsType) {
+    return axiosInstance.post(`/${PATH}/${postId}/comment/add`);
+  },
+  postRecomment({ postId }: commentsType) {
+    return axiosInstance.post(`/${PATH}/${postId}/recomment/add`);
+  },
+};
+
+export default CommentsApi;
