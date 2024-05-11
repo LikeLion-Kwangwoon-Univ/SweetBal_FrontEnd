e4c018bf63687ab42c2fe5a740c2b58988199789
김동균 <wooheuy5752@nate.com>
2024-05-11 13:58:15 +0900

    Fix : msw handlers 주석 제거

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -11,28 +11,3 @@ export const handlers = [
   ...getBalanceData,
   ...postPickData,
 ];
-
-//   // 댓글 좋아요 추가
-//   http.patch("/comments/1/liked/1", async () => {
-//     const commentIndex = comments.findIndex((c) => c.id === "1");
-//     if (commentIndex !== -1) {
-//       comments[commentIndex] = {
-//         ...comments[commentIndex],
-//         liked: comments[commentIndex].liked + 1,
-//       };
-//     }
-//     return HttpResponse.json(comments);
-//   }),
-
-//   // 댓글 좋아요 삭제
-//   http.patch("/comments/1/unliked/1", async () => {
-//     const commentIndex = comments.findIndex((c) => c.id === "1");
-//     if (commentIndex !== -1) {
-//       comments[commentIndex] = {
-//         ...comments[commentIndex],
-//         liked: comments[commentIndex].liked - 1,
-//       };
-//     }
-//     return HttpResponse.json(comments);
-//   }),
-// ];
