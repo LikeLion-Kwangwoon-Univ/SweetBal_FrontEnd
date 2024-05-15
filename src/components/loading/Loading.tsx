6e816d29d2d2c4ff683c3b89bc0f08f8f48b76bc
teagu123 <103398790+teagu123@users.noreply.github.com>
2024-05-15 14:11:23 +0900

    Feature/main page (#41)
    * main 변경사항 추가 적용 (#19)
    
    * 메인 페이지 스타일 및 공용 컴포넌트 구현 (#9)
    
    * Design : MainPage Style중
    
    * Design : root header변경
    
    * Design : NavBar 스타일 구현 완료
    
    * Design : global css 변경
    
    * Design : Comments Btn 스타일 구현
    
    * Design : New Btn 스타일 구현
    
    * Design : navBar 스타일 구현
    
    * Design : global css 수정
    
    * Refactor: update UI & refac code
    
    * Refactor: refactory code
    
    * Style: 헤더 이미지 삽입
    
    * Feature/listpage (#12)
    
    * Design: Error 스타일 구현
    
    * Design: loading 스타일 구현
    
    * Design: list페이지 스타일 구현
    
    * Design: Loading 컴포넌트 스피너, 크기 수정
    
    ---------
    
    Co-authored-by: teagu123 <103398790+teagu123@users.noreply.github.com>
    
    * Fix : Merge과정에서 오류 수정
    
    * Design : global css 변경
    
    * Feat : 댓글창 구현 및 balance 페이지와 병합 완료 (#16)
    
    * Feat : 댓글 판넬 및 헤더 생성
    
    * Style : 댓글 창 padding 추가
    
    * Feat : 말풍선 생성
    
    * Feat : 댓글 및 대댓글 탭 UI 완성
    
    * FIX : 댓글 및 대댓글 map 메소드 key 값 index로 임시 설정
    
    * Fix: 댓글 창에 아무것도 입력하지 않고 전송하는 경우 처리
    
    * Feat : useScrollToBottom  커스텀 훅 생성
    
    ref를 이용하여 댓글 입력 시, 댓글 창 최하단으로 이동
    
    * Refactor : 댓글 순서 최신순으로 설정
    
    useScrollToBottom 훅과 msw handler 수정
    
    * Style : 댓글 글자 크기 수정
    
    * Feat : 좋아요 기능 추가
    
    * Fix : react-icons import 수정
    
    * Refactor : 뷰포트 크기에 맞도록 레이아웃 수정
    
    * Style: 댓글 창 내부 크기 수정
    
    * Rename: useScrollToTop 이름 변경
    
    * Feat: 댓글 창 닫기 기능 구현
    
    * Style : 하단 메인 프레임 전역 padding 추가
    
    ---------
    
    Co-authored-by: teagu123 <103398790+teagu123@users.noreply.github.com>
    
    * 밸런스게임 생성 페이지 구현 (#17)
    
    * Design : style오류 수정 (#18)
    
    * feat : router register 추가
    
    * package.json 수정
    
    * Fix : rollup window 추가
    
    ---------
    
    Co-authored-by: g0mteng <gomteng03@naver.com>
    Co-authored-by: 김동균 <104538667+KimDongGyun23@users.noreply.github.com>
    Co-authored-by: 김동균 <wooheuy5752@nate.com>
    Co-authored-by: HANTAEDONG <132195232+HANTAEDONG@users.noreply.github.com>
    Co-authored-by: huzan2 <95648841+huzan2@users.noreply.github.com>
    
    * Refactor : MSW, API, Query
    
    * Fix : 에러 수정
    
    * Design : Loading background변경
    
    * Feat : Msw최종 수정
    
    * Feat : data 적용까지 완료
    
    * Design : 15글자 이상시 .. 아닐시 그냥 출력
    
    * Refactor : utils strAddDots함수 추가
    
    ---------
    
    Co-authored-by: g0mteng <gomteng03@naver.com>
    Co-authored-by: 김동균 <104538667+KimDongGyun23@users.noreply.github.com>
    Co-authored-by: 김동균 <wooheuy5752@nate.com>
    Co-authored-by: HANTAEDONG <132195232+HANTAEDONG@users.noreply.github.com>
    Co-authored-by: huzan2 <95648841+huzan2@users.noreply.github.com>

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -2,28 +2,29 @@ import styled from "styled-components";
 import { FlexCenterCSS, FlexColumnCSS } from "../../styles/common";
 import { SyncLoader } from "react-spinners";
 import React from "react";
+
 // import Logo from "../../assets/loading/logo.svg";
 const LoadingPage = () => {
-  return (
-    <Border>
-      <Container>
-        {/* <LogoWrapper>
+	return (
+		<Border>
+			<Container>
+				{/* <LogoWrapper>
           <img src={Logo}></img>
         </LogoWrapper> */}
-        <WaitSpan>잠시만 기다리세요</WaitSpan>
-        <SipnnerWrapper>
-          <SyncLoader color="black" size={15} />
-        </SipnnerWrapper>
-      </Container>
-    </Border>
-  );
-};
+				<WaitSpan>잠시만 기다리세요</WaitSpan>
+				<SipnnerWrapper>
+					<SyncLoader color="black" size={15} />
+				</SipnnerWrapper>
+			</Container>
+		</Border>
+	)
+}
 
-export default LoadingPage;
+export default LoadingPage
 
 const Border = styled.div`
-  ${FlexCenterCSS};
-`;
+	${FlexCenterCSS};
+`
 
 const Container = styled.div`
   ${FlexColumnCSS};
@@ -45,13 +46,13 @@ const Container = styled.div`
 // `;
 
 const SipnnerWrapper = styled.div`
-  margin-top: 25px;
-`;
+	margin-top: 25px;
+`
 
 const WaitSpan = styled.span`
-  font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
-  margin-top: 20px;
-  margin: 0 auto;
-  color: black;
-  font-size: 25px;
-`;
+	font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
+	margin-top: 20px;
+	margin: 0 auto;
+	color: black;
+	font-size: 25px;
+`
