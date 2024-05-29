1d05a0cbbba22adbbbf6b1555860bd2e66168807
김동균 <wooheuy5752@nate.com>
2024-05-29 20:55:51 +0900

    FIX : main 페이지와 댓글 창 api 수정

--- src/pages/main/components/singleListBox.tsx
+++ src/pages/main/components/singleListBox.tsx
@@ -1,39 +1,40 @@
-import { useNavigate } from 'react-router-dom'
-import { styled } from 'styled-components'
-import { listType } from '../../../query/get/useGetMain'
-import strAddDots from '../../../utils/strAddDots'
+import { useNavigate } from "react-router-dom";
+import { styled } from "styled-components";
+import { listType } from "../../../query/get/useGetMain";
+import strAddDots from "../../../utils/strAddDots";
 
 function SingleListBox({ item }: { item: listType }) {
-	const navigate = useNavigate()
+  const navigate = useNavigate();
+  console.log(item);
 
-	return (
-		<Container onClick={() => navigate('/balance')}>
-			<>
-				<Title>{strAddDots(item.leftSideTitle)}</Title>
-				<VS>VS</VS>
-				<Title>{strAddDots(item.rightSideTitle)}</Title>
-			</>
-		</Container>
-	)
+  return (
+    <Container onClick={() => navigate(`/balance/${item.id}`)}>
+      <>
+        <Title>{strAddDots(item.leftSideTitle)}</Title>
+        <VS>VS</VS>
+        <Title>{strAddDots(item.rightSideTitle)}</Title>
+      </>
+    </Container>
+  );
 }
-export default SingleListBox
+export default SingleListBox;
 
 const Container = styled.div`
-	background-color: ${({ theme }) => theme.COLOR.blue1};
-	margin: 12px 12px;
-	padding: 10px;
-	border-radius: 10px;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	cursor: pointer;
-`
+  background-color: ${({ theme }) => theme.COLOR.blue1};
+  margin: 12px 12px;
+  padding: 10px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+`;
 const Title = styled.div`
-	font-size: 12px;
-	width: 100%;
-	text-align: center;
-`
+  font-size: 12px;
+  width: 100%;
+  text-align: center;
+`;
 const VS = styled.div`
-	font-size: 20px;
-	font-weight: bold;
-`
+  font-size: 20px;
+  font-weight: bold;
+`;
