2b475da659dd7ddcd816f5ba05c7559643c1593a
김동균 <wooheuy5752@nate.com>
2024-05-31 21:31:35 +0900

    Style : 밸런스 페이지 ui 수정

--- src/pages/balance/Balance.tsx
+++ src/pages/balance/Balance.tsx
@@ -1,130 +1,130 @@
-import VSImg from '../../assets/imgs/VSImg.svg'
-import { useState } from 'react'
-import SubTitleContent from './SubTitleContent'
-import { styled, keyframes } from 'styled-components'
-import { FlexCenterCSS, FlexColumnCSS } from '../../styles/common'
-import BalanceApi from '../../apis/balanceApi'
-import useGetBalanceData from '../../query/get/useGetBalance'
-import Comments from './components/Comments'
-import NewBtn from '../../components/Button/newBtn'
-import NavBar from '../../components/navBar/navBar'
-import { useRecoilValue, useSetRecoilState } from 'recoil'
-import { isOpenCommentState, selectsState } from '@/store/comments/atoms'
-import { useParams } from 'react-router-dom'
+import VSImg from "../../assets/imgs/VSImg.svg";
+import { useState } from "react";
+import SubTitleContent from "./SubTitleContent";
+import { styled, keyframes } from "styled-components";
+import { FlexCenterCSS, FlexColumnCSS } from "../../styles/common";
+import BalanceApi from "../../apis/balanceApi";
+import useGetBalanceData from "../../query/get/useGetBalance";
+import Comments from "./components/Comments";
+import NewBtn from "../../components/Button/newBtn";
+import NavBar from "../../components/navBar/navBar";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { isOpenCommentState, selectsState } from "@/store/comments/atoms";
+import { useParams } from "react-router-dom";
 
 function BalancePage() {
-    const { id, subject } = {
-        id: Number(useParams<{ id: string }>().id),
-        subject: useParams<{ subject: string }>().subject,
+  const { id, subject } = {
+    id: Number(useParams<{ id: string }>().id),
+    subject: useParams<{ subject: string }>().subject,
+  };
+  const { data: gameData, status } = useGetBalanceData(id);
+  const [select, setSelect] = useState("");
+  const [percent, setPercent] = useState(0);
+  const setSelectState = useSetRecoilState(selectsState);
+  const isOpenComment = useRecoilValue(isOpenCommentState);
+
+  const handleSelect = async (userSelect: string) => {
+    if (select === "") {
+      setSelect(userSelect);
+      setSelectState(userSelect);
+      await BalanceApi.postPick(id, userSelect);
+      const total = gameData!.leftSideVote + gameData!.rightSideVote + 1;
+      const leftVote =
+        userSelect === "left"
+          ? gameData!.leftSideVote + 1
+          : gameData!.leftSideVote;
+      setPercent(Math.round((leftVote / total) * 100));
     }
-    const { data: gameData, status } = useGetBalanceData(id)
-    const [select, setSelect] = useState('')
-    const [percent, setPercent] = useState(0)
-    const setSelectState = useSetRecoilState(selectsState)
-    const isOpenComment = useRecoilValue(isOpenCommentState)
+  };
 
-    const handleSelect = async (userSelect: string) => {
-        if (select === '') {
-            setSelect(userSelect)
-            setSelectState(userSelect)
-            await BalanceApi.postPick(id, userSelect)
-            const total = gameData!.leftSideVote + gameData!.rightSideVote + 1
-            const leftVote =
-                userSelect === 'left'
-                    ? gameData!.leftSideVote + 1
-                    : gameData!.leftSideVote
-            setPercent(Math.round((leftVote / total) * 100))
-        }
-    }
+  if (isOpenComment) return <Comments />;
+  if (status === "loading") return <div>로딩중</div>;
+  if (status === "error") return <div>에러가 발생했습니다</div>;
 
-    if (isOpenComment) return <Comments />
-    if (status === 'loading') return <div>로딩중</div>
-    if (status === 'error') return <div>에러가 발생했습니다</div>
-
-    return (
-        <Border>
-            <NavBar
-                title={`${subject} balance` || 'balance game'}
-                url="/"
-                views={gameData.leftSideVote + gameData.rightSideVote}
-            ></NavBar>
-            <Wrapper>
-                <TitleBox onClick={() => handleSelect('left')}>
-                    {gameData.leftSideTitle}
-                </TitleBox>
-                {select ? (
-                    <PercentWrapper>
-                        <PercentBox percent={`${percent}%`}>
-                            {percent}
-                        </PercentBox>
-                    </PercentWrapper>
-                ) : (
-                    <div style={{ height: '36px' }}></div>
-                )}
-                <SubTitleContent
-                    content={gameData.leftSideDetail}
-                ></SubTitleContent>
-                <ImgWrapper>
-                    <img src={VSImg}></img>
-                </ImgWrapper>
-                <TitleBox onClick={() => handleSelect('right')}>
-                    {gameData.rightSideTitle}
-                </TitleBox>
-                {select ? (
-                    <PercentWrapper>
-                        <PercentBox percent={`${100 - percent}%`}>
-                            {100 - percent}
-                        </PercentBox>
-                    </PercentWrapper>
-                ) : (
-                    <div style={{ height: '36px' }}></div>
-                )}
-                <SubTitleContent
-                    content={gameData.rightSideDetail}
-                ></SubTitleContent>
-            </Wrapper>
-            <NewBtn />
-        </Border>
-    )
+  return (
+    <Border>
+      <NavBar
+        title={`${subject} balance` || "balance game"}
+        url="/"
+        views={gameData.leftSideVote + gameData.rightSideVote}
+      ></NavBar>
+      <Wrapper>
+        <Content>
+          <TitleBox onClick={() => handleSelect("left")}>
+            {gameData.leftSideTitle}
+          </TitleBox>
+          {select ? (
+            <PercentWrapper>
+              <PercentBox percent={`${percent}%`}>{percent}</PercentBox>
+            </PercentWrapper>
+          ) : (
+            <div style={{ height: "36px" }}></div>
+          )}
+          <SubTitleContent content={gameData.leftSideDetail} />
+          <ImgWrapper>
+            <img src={VSImg}></img>
+          </ImgWrapper>
+          <TitleBox onClick={() => handleSelect("right")}>
+            {gameData.rightSideTitle}
+          </TitleBox>
+          {select ? (
+            <PercentWrapper>
+              <PercentBox percent={`${100 - percent}%`}>
+                {100 - percent}
+              </PercentBox>
+            </PercentWrapper>
+          ) : (
+            <div style={{ height: "36px" }}></div>
+          )}
+          <SubTitleContent content={gameData.rightSideDetail} />
+        </Content>
+      </Wrapper>
+      <NewBtn />
+    </Border>
+  );
 }
 
-export default BalancePage
+export default BalancePage;
 
 const Border = styled.div`
-    box-sizing: border-box;
-    overflow: hidden;
-    position: relative;
-    min-height: calc(100vh - 132px - 20px);
-    background-color: white;
-    border-radius: 13px;
-    ${FlexColumnCSS}
-    align-items: center;
-`
+  box-sizing: border-box;
+  overflow: hidden;
+  position: relative;
+  min-height: calc(100vh - 132px - 20px);
+  background-color: white;
+  border-radius: 13px;
+  ${FlexColumnCSS}
+  align-items: center;
+`;
 
 const Wrapper = styled.div`
-    flex-grow: 1;
-    width: 285px;
-    ${FlexColumnCSS};
-    justify-content: space-between;
-    align-items: center;
-    padding: 87px 0 108px 0;
-`
+  flex-grow: 1;
+  width: 285px;
+  ${FlexCenterCSS};
+  padding: 87px 0 108px 0;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const TitleBox = styled.div`
-    width: 100%;
-    height: 107px;
-    background-color: ${({ theme }) => theme.COLOR.blue4};
-    border-radius: 13px;
-    color: white;
-    font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
-    font-size: 30px;
-    ${FlexCenterCSS}
-`
+  width: 100%;
+  height: 107px;
+  background-color: ${({ theme }) => theme.COLOR.blue4};
+  border-radius: 13px;
+  color: white;
+  font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
+  font-size: 30px;
+  ${FlexCenterCSS}
+`;
 
 const ImgWrapper = styled.div`
-    width: 73px;
-    height: 107px;
-`
+  width: 73px;
+  height: 107px;
+`;
 
 const fillAnimation = keyframes<{ percent: string }>`
   from {
@@ -133,21 +133,21 @@ const fillAnimation = keyframes<{ percent: string }>`
   to {
     width: ${({ percent }) => percent};
   }
-`
+`;
 
 const PercentBox = styled.div<{ percent: string }>`
-    width: ${({ percent }) => percent};
-    animation: ${({ percent }) => fillAnimation} 2s ease-out;
-    margin: 5px 0px;
-    height: 26px;
-    background-color: #f9cccc;
-    border-radius: 3px;
-    text-align: center;
-    font-size: 18px;
-    padding-top: 6px;
-    box-sizing: border-box;
-`
+  width: ${({ percent }) => percent};
+  animation: ${({ percent }) => fillAnimation} 2s ease-out;
+  margin: 5px 0px;
+  height: 26px;
+  background-color: #f9cccc;
+  border-radius: 3px;
+  text-align: center;
+  font-size: 18px;
+  padding-top: 6px;
+  box-sizing: border-box;
+`;
 
 const PercentWrapper = styled.div`
-    width: 100%;
-`
+  width: 100%;
+`;
--- src/pages/balance/BalanceStyle.ts
+++ src/pages/balance/BalanceStyle.ts
@@ -1,74 +1,74 @@
-import { styled, keyframes } from 'styled-components'
-import { FlexCenterCSS, FlexColumnCSS } from '../../styles/common'
+import { styled, keyframes } from "styled-components";
+import { FlexCenterCSS, FlexColumnCSS } from "../../styles/common";
 
 export const Border = styled.div`
-    box-sizing: border-box;
-    overflow: hidden;
-    position: relative;
-    min-height: calc(100vh - 132px - 20px);
-    background-color: white;
-    border-radius: 13px;
-    ${FlexColumnCSS}
-    align-items: center;
-`
+  box-sizing: border-box;
+  overflow: hidden;
+  position: relative;
+  min-height: calc(100vh - 132px - 20px);
+  background-color: white;
+  border-radius: 13px;
+  ${FlexColumnCSS}
+  align-items: center;
+`;
 
 export const Wrapper = styled.div`
-    flex-grow: 1;
-    width: 285px;
-    ${FlexColumnCSS};
-    justify-content: space-between;
-    align-items: center;
-`
+  flex-grow: 1;
+  width: 285px;
+  ${FlexColumnCSS};
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export const Bar = styled.div`
-    width: 321px;
-    height: 19px;
-    display: flex;
-    justify-content: space-between;
-    font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
-`
+  width: 321px;
+  height: 19px;
+  display: flex;
+  justify-content: space-between;
+  font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
+`;
 
 export const SubjectText = styled.span`
-    font-size: 16px;
-    padding-left: 5px;
-`
+  font-size: 16px;
+  padding-left: 5px;
+`;
 
 export const EyesScore = styled.span`
-    font-size: 12px;
-    padding-left: 5px;
-    margin-top: 2px;
-`
+  font-size: 12px;
+  padding-left: 5px;
+  margin-top: 2px;
+`;
 
 export const SizedBox = styled.div<{ hSize: string }>`
-    height: ${({ hSize }) => hSize};
-`
+  height: ${({ hSize }) => hSize};
+`;
 
 export const GameWrapper = styled.div`
-    width: 285px;
-    ${FlexColumnCSS}
-    align-items: center;
-`
+  width: 285px;
+  ${FlexColumnCSS}
+  align-items: center;
+`;
 
 export const TitleBox = styled.div`
-    width: 100%;
-    height: 107px;
-    background-color: ${({ theme }) => theme.COLOR.blue4};
-    border-radius: 13px;
-    color: white;
-    font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
-    font-size: 30px;
-    ${FlexCenterCSS}
-`
+  width: 100%;
+  height: 107px;
+  background-color: ${({ theme }) => theme.COLOR.blue4};
+  border-radius: 13px;
+  color: white;
+  font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
+  font-size: 30px;
+  ${FlexCenterCSS}
+`;
 
 export const ImgWrapper = styled.div`
-    width: 73px;
-    height: 107px;
-`
+  width: 73px;
+  height: 107px;
+`;
 
 export const SpanWrapper = styled.span`
-    display: flex;
-    align-items: center;
-`
+  display: flex;
+  align-items: center;
+`;
 const fillAnimation = keyframes<{ percent: string }>`
   from {
     width: 0%;
@@ -76,21 +76,21 @@ const fillAnimation = keyframes<{ percent: string }>`
   to {
     width: ${({ percent }) => percent};
   }
-`
+`;
 
 export const PercentBox = styled.div<{ percent: string }>`
-    width: ${({ percent }) => percent};
-    animation: ${({ percent }) => fillAnimation} 2s ease-out;
-    margin: 5px 0px;
-    height: 26px;
-    background-color: #f9cccc;
-    border-radius: 3px;
-    text-align: center;
-    font-size: 18px;
-    padding-top: 6px;
-    box-sizing: border-box;
-`
+  width: ${({ percent }) => percent};
+  animation: ${({ percent }) => fillAnimation} 2s ease-out;
+  margin: 5px 0px;
+  height: 26px;
+  background-color: #f9cccc;
+  border-radius: 3px;
+  text-align: center;
+  font-size: 18px;
+  padding-top: 6px;
+  box-sizing: border-box;
+`;
 
 export const PercentWrapper = styled.div`
-    width: 100%;
-`
+  width: 100%;
+`;
