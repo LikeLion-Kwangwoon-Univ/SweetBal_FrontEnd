a0d6cdbd1e3d810b13a13907863a8730be1ba437
김동균 <wooheuy5752@nate.com>
2024-06-01 15:32:06 +0900

    Feat : 에러 페이지 네비게이션 설정

--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -1,107 +1,110 @@
-import styled from 'styled-components'
-import Outlet from '../../assets/imgs/Error/OuletImg.svg'
-import Error from '../../assets/imgs/Error/ErrorImg.svg'
-import VsImg from '../../assets/imgs/Error/VsImg.svg'
-import ErrorRbot from '../../assets/imgs/Error/ErrorRobot.svg'
-import { FlexCenterCSS, FlexColumnCSS } from '../../styles/common'
+import styled from "styled-components";
+import Outlet from "../../assets/imgs/Error/OuletImg.svg";
+import Error from "../../assets/imgs/Error/ErrorImg.svg";
+import VsImg from "../../assets/imgs/Error/VsImg.svg";
+import ErrorRbot from "../../assets/imgs/Error/ErrorRobot.svg";
+import { FlexCenterCSS, FlexColumnCSS } from "../../styles/common";
+import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
-	return (
-		<Border>
-			<Container>
-				<OuletWrapper>
-					<img src={Outlet}></img>
-				</OuletWrapper>
-				<ErrorWrapper>
-					<img src={Error}></img>
-				</ErrorWrapper>
-				<GameWrapper>
-					<TitleBox>메인 페이지 돌아가기</TitleBox>
-					<SubTitleContent>차분한 마음으로 돌아가기</SubTitleContent>
-					<ImgWrapper>
-						<img src={VsImg}></img>
-					</ImgWrapper>
-					<TitleBox>새로고침 하기</TitleBox>
-					<SubTitleContent>그런다고 될까...?</SubTitleContent>
-				</GameWrapper>
-				<ErrorRobotWrapper>
-					<img src={ErrorRbot}></img>
-				</ErrorRobotWrapper>
-				{/* <LoadingPage /> */}
-			</Container>
-		</Border>
-	)
-}
+  const navigate = useNavigate();
 
-export default ErrorPage
+  return (
+    <Border>
+      <Container>
+        <OuletWrapper>
+          <img src={Outlet}></img>
+        </OuletWrapper>
+        <ErrorWrapper>
+          <img src={Error}></img>
+        </ErrorWrapper>
+        <GameWrapper>
+          <TitleBox onClick={() => navigate("/")}>
+            메인 페이지 돌아가기
+          </TitleBox>
+          <SubTitleContent>차분한 마음으로 돌아가기</SubTitleContent>
+          <ImgWrapper>
+            <img src={VsImg}></img>
+          </ImgWrapper>
+          <TitleBox onClick={() => navigate(-1)}>뒤로 돌아가기</TitleBox>
+          <SubTitleContent>과연 뒤가 어디일까?</SubTitleContent>
+        </GameWrapper>
+        <ErrorRobotWrapper>
+          <img src={ErrorRbot}></img>
+        </ErrorRobotWrapper>
+      </Container>
+    </Border>
+  );
+};
+
+export default ErrorPage;
 
 const Border = styled.div`
-	${FlexCenterCSS};
-	margin-top: 50px;
-`
+  ${FlexCenterCSS};
+`;
 
 const Container = styled.div`
-	${FlexColumnCSS};
-	align-items: center;
-	background-color: white;
-	display: flex;
-	width: 350px;
-	margin: 0 auto;
-	height: fit-content;
-	border-radius: 13px;
-`
+  ${FlexColumnCSS};
+  align-items: center;
+  background-color: white;
+  display: flex;
+  width: 350px;
+  margin: 0 auto;
+  height: fit-content;
+  border-radius: 13px;
+`;
 
 const OuletWrapper = styled.div`
-	width: 351px;
-	height: 34px;
-`
+  width: 351px;
+  height: 34px;
+`;
 
 const ErrorWrapper = styled.div`
-	width: 203px;
-	height: 95px;
-	${FlexCenterCSS}
-`
+  width: 203px;
+  height: 95px;
+  ${FlexCenterCSS}
+`;
 
 const GameWrapper = styled.div`
-	width: 285px;
-	${FlexColumnCSS}
-	align-items: center;
-	cursor: pointer;
-`
+  width: 285px;
+  ${FlexColumnCSS}
+  align-items: center;
+  cursor: pointer;
+`;
 
 const TitleBox = styled.div`
-	width: 100%;
-	height: 107px;
-	margin-bottom: 9px;
-	background-color: ${({ theme }) => theme.COLOR.blue4};
-	border-radius: 13px;
-	color: white;
-	font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
-	font-size: 30px;
-	${FlexCenterCSS}
-`
+  width: 100%;
+  height: 107px;
+  margin-bottom: 9px;
+  background-color: ${({ theme }) => theme.COLOR.blue4};
+  border-radius: 13px;
+  color: white;
+  font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
+  font-size: 25px;
+  ${FlexCenterCSS}
+`;
 
 const ImgWrapper = styled.div`
-	width: 73px;
-	height: 107px;
-	${FlexCenterCSS}
-`
+  width: 73px;
+  height: 107px;
+  ${FlexCenterCSS}
+`;
 
 const SubTitleContent = styled.div`
-	width: 100%;
-	height: 47px;
-	background-color: ${({ theme }) => theme.COLOR.blue1};
-	border-radius: 13px;
-	color: ${({ theme }) => theme.COLOR.grey1};
-	font-size: 16px;
-	font-family: ${({ theme }) => theme.FONT_WEIGHT.regular};
-	${FlexCenterCSS}
-	text-align: center;
-`
+  width: 100%;
+  height: 47px;
+  background-color: ${({ theme }) => theme.COLOR.blue1};
+  border-radius: 13px;
+  color: ${({ theme }) => theme.COLOR.grey1};
+  font-size: 16px;
+  font-family: ${({ theme }) => theme.FONT_WEIGHT.regular};
+  ${FlexCenterCSS}
+  text-align: center;
+`;
 
 const ErrorRobotWrapper = styled.div`
-	width: 130px;
-	height: 95px;
-	margin-top: 11px;
-	align-self: flex-end;
-`
+  width: 130px;
+  height: 95px;
+  margin-top: 11px;
+  align-self: flex-end;
+`;
