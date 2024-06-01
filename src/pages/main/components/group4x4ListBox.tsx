3be539d3037416c96a132555fb30f117c30172f8
김동균 <wooheuy5752@nate.com>
2024-06-01 12:31:45 +0900

    Style : 메인 페이지 스타일 수정

--- src/pages/main/components/group4x4ListBox.tsx
+++ src/pages/main/components/group4x4ListBox.tsx
@@ -1,48 +1,65 @@
-import { styled } from 'styled-components'
-import SingleListBox from './singleListBox'
-import { IoIosArrowForward } from 'react-icons/io'
-import { responseType } from '../../../query/get/useGetMain'
-import { useNavigate } from 'react-router-dom'
+import { styled } from "styled-components";
+import SingleListBox from "./singleListBox";
+import { IoIosArrowForward } from "react-icons/io";
+import { responseType } from "../../../query/get/useGetMain";
+import { useNavigate } from "react-router-dom";
 
 function Group4x4ListBox({ subject, postList }: responseType) {
-	const navigate = useNavigate()
+  const navigate = useNavigate();
 
-	return (
-		<Container>
-			<Title onClick={() => navigate(`/list/${subject.split(' ')[0]}`)}>
-				{subject}
-				<IoIosArrowForward />
-			</Title>
-			<>
-				{postList.map((item, idx) => (
-					<SingleListBox
-						key={idx}
-						item={item}
-						subject={subject.split(' ')[0]}
-					/>
-				))}
-			</>
-		</Container>
-	)
+  return (
+    <Container>
+      <Title onClick={() => navigate(`/list/${subject.split(" ")[0]}`)}>
+        {subject}
+        <IoIosArrowForward />
+      </Title>
+
+      <ListContainer>
+        {postList.length === 0 ? (
+          <p className="no-data">아직 박빙인 게임이 없어요.</p>
+        ) : (
+          postList.map((item, idx) => (
+            <SingleListBox
+              key={idx}
+              item={item}
+              subject={subject.split(" ")[0]}
+            />
+          ))
+        )}
+      </ListContainer>
+    </Container>
+  );
 }
-export default Group4x4ListBox
+export default Group4x4ListBox;
 
 const Container = styled.div`
-	height: 250px;
-	border-radius: 15px;
-	background-color: white;
-	margin-top: 20px;
-`
-const Title = styled.div`
-	font-size: 20px;
-	font-weight: bold;
-	margin: 10px;
-	display: flex;
-	align-items: center;
-	* {
-		margin-left: 5px;
-	}
-	cursor: pointer;
+  padding: 12px;
+  border-radius: 15px;
+  background-color: white;
+`;
 
-	width: fit-content;
-`
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  margin-top: 12px;
+
+  .no-data {
+    padding: 12px;
+    text-align: center;
+    font-size: 14px;
+  }
+`;
+
+const Title = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  * {
+    margin-left: 5px;
+  }
+  cursor: pointer;
+
+  width: fit-content;
+`;
